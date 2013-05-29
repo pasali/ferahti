5e51e9bf9066df467c5958fe33bab12736ab3a86
pasali <mhmtbsl@bil.omu.edu.tr>
2013-05-30 00:43:03 +0300

    artık

---
+++ src/main/java/servlet/AraclarHandler.java
@@ -0,0 +1,133 @@
+package main;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import main.Araclar;
+
+
+public class AraclarHandler {
+	static final String URL = "jdbc:mysql://localhost/ferahti?useUnicode=true&characterEncoding=UTF-8";
+	static final String USER = "root";
+	static final String PASS = "4ty9";
+	private Connection  conn;
+	private Statement stmt;
+	private ResultSet rs;
+	
+	public void AracEkle(Araclar arac) {
+		
+		String sorgu = "INSERT INTO `ferahti`.`arac` (`marka`, `model`, `yakit`) " +
+				"VALUES ('"+ arac.getMarka() +"', '" + arac.getPlaka() + "', '"+ arac.getDurum() +"')";
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			conn = DriverManager.getConnection(URL, USER, PASS);
+			stmt = conn.createStatement();
+			stmt.executeUpdate(sorgu);
+			
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+		
+	public void AracGuncelle(String id, String user, String status) {
+		
+		String sorgu = "UPDATE `ferahti`.`arac` SET `kullanici`='" + user + "', `durum`='" + status + "' WHERE `idarac`='"+ id + "';";
+		
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			conn = DriverManager.getConnection(URL, USER, PASS);
+			stmt = conn.createStatement();
+			stmt.executeUpdate(sorgu);
+			
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public ArrayList<Araclar> AraclariGetir() {
+		ArrayList<Araclar> araclistesi = new ArrayList<Araclar>();
+		String sorgu = "select * from ferahti.arac";
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			conn = DriverManager.getConnection(URL, USER, PASS);
+			stmt = conn.createStatement();
+			stmt.executeQuery(sorgu);
+			rs = stmt.executeQuery(sorgu);
+			while (rs.next()) {
+				Araclar a = new Araclar(rs.getInt("idarac"), rs.getString("marka"),rs.getString("kullanici"), rs.getString("plaka"), rs.getString("durum"));
+				araclistesi.add(a);
+			}
+			
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return araclistesi;
+	}
+	public void AracSil(String id) {
+		String sorgu = "DELETE FROM `ferahti`.`arac` WHERE `idarac`='"+ id +"';";
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			conn = DriverManager.getConnection(URL, USER, PASS);
+			stmt = conn.createStatement();
+			stmt.executeUpdate(sorgu);
+			
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	public ArrayList<Araclar> BosAraclariGetir() {
+		ArrayList<Araclar> araclistesi = new ArrayList<Araclar>();
+		String sorgu = "select * from ferahti.arac";
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			conn = DriverManager.getConnection(URL, USER, PASS);
+			stmt = conn.createStatement();
+			stmt.executeQuery(sorgu);
+			rs = stmt.executeQuery(sorgu);
+			while (rs.next()) {
+				Araclar a = new Araclar(rs.getInt("idarac"), rs.getString("marka"),rs.getString("kullanici"), rs.getString("plaka"), rs.getString("durum"));
+				if (rs.getString("durum").equals("boşta")) {
+					araclistesi.add(a);
+				}
+				
+			}
+			
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return araclistesi;
+	}
+	
+	public ArrayList<Araclar> KullanimdakiAraclariGetir(String user) {
+		ArrayList<Araclar> araclistesi = new ArrayList<Araclar>();
+		String sorgu = "select * from ferahti.arac";
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			conn = DriverManager.getConnection(URL, USER, PASS);
+			stmt = conn.createStatement();
+			stmt.executeQuery(sorgu);
+			rs = stmt.executeQuery(sorgu);
+			while (rs.next()) {
+				Araclar a = new Araclar(rs.getInt("idarac"), rs.getString("marka"),rs.getString("kullanici"), rs.getString("plaka"), rs.getString("durum"));
+				if (rs.getString("kullanici").equals(user)) {
+					araclistesi.add(a);
+				}
+				
+			}
+			
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return araclistesi;
+	}
+	
+	
+}
